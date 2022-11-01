b799fac00996d38697198e7feddecd6e630ebd8c
Henriqque <hdoido1992@gmail.com>
2022-11-01 15:05:08 -0300

    first commited

---
+++ index.js
@@ -0,0 +1,60 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const axios = require("axios");
+const express = require("express");
+const bodyParser = require("body-parser");
+const GNRequest = require("./src/apis/gerencianet");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/payments/cob/pix", async (req, res) => {
+  const reqGN = await GNRequest({
+    clientID: process.env.GN_CLIENT_ID,
+    clientSecret: process.env.GN_CLIENT_SECRET,
+  });
+  const { fullname, expire, amount, cpf } = req.body;
+  const dataCob = {
+    calendario: {
+      expiracao: expire,
+    },
+    devedor: {
+      cpf: cpf,
+      nome: fullname,
+    },
+    valor: {
+      original: amount.toFixed(2),
+    },
+    chave: "3e4639c6-3267-4335-8c01-d309525e21ec",
+    solicitacaoPagador: "Informe o número ou identificador do pedido.",
+  };
+
+  const cobResponse = await reqGN.post("v2/cob", dataCob);
+  const qrcodeResponse = await reqGN.get(
+    `/v2/loc/${cobResponse.data.loc.id}/qrcode`,
+    dataCob
+  );
+
+  const data = {
+    total: amount,
+    qrcode: qrcodeResponse.data,
+    status: "pending",
+    txid: cobResponse.data.txid,
+  };
+
+  res.send(data);
+});
+
+app.post("/webhook(/pix)?", async (req, res) => {
+  console.log(req.body);
+  const { pix } = req.body;
+  try {
+    return console.log("sucess");
+  } catch (error) {
+    console.log(error);
+  }
+  res.send(200);
+});
+
+app.listen(8000, () => console.log("server running"));
